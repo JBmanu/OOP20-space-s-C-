1e674a923de4a8a3d3237efa494fd2e7f6631ca9
Bmanuel <buizomanuel@gmail.com>
2021-07-06 12:17:06 +0200

    finish task c#

---
+++ Buizo/OOP20-SpaceSurvival-CSharp/OOP20-SpaceSurvival-CSharp/src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP20_SpaceSurvival_CSharp.src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Buizo/OOP20-SpaceSurvival-CSharp/Test/TestSound.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+
+using OOP20_SpaceSurvival_CSharp.gui.sound;
+using OOP20_SpaceSurvival_CSharp.gui;
+
+namespace Test.Properties
+{
+    public class Tests
+    {
+
+        [Test]
+        public void ControlEngineSoundFields()
+        {
+            EngineSound sound = new EngineSound();
+
+            Assert.AreEqual(sound.GetTitle(), "SOUND");
+            Assert.AreEqual(sound.GetVisibility(), Visibility.HIDDEN);
+        }
+
+        [Test]
+        public void CheckValueSound()
+        {
+            EngineSound sound = new EngineSound();
+
+            Assert.AreEqual(sound.GetValueUnitSound(TypeUnitSound.SLIDER_BACKGROUND), 50);
+            Assert.AreEqual(sound.GetValueUnitSound(TypeUnitSound.SLIDER_EFFECT), 50);
+        }
+
+        [Test]
+        public void CheckChangeValueSoundOfType()
+        {
+            EngineSound sound = new EngineSound();
+
+            Assert.AreEqual(sound.GetValueUnitSound(TypeUnitSound.SLIDER_BACKGROUND), 50);
+            Assert.AreEqual(sound.GetValueUnitSound(TypeUnitSound.SLIDER_EFFECT), 50);
+
+            sound.SetValueUnitSound(TypeUnitSound.SLIDER_BACKGROUND, 100);
+            sound.SetValueUnitSound(TypeUnitSound.SLIDER_EFFECT, 100);
+
+            Assert.AreEqual(sound.GetValueUnitSound(TypeUnitSound.SLIDER_BACKGROUND), 100);
+            Assert.AreEqual(sound.GetValueUnitSound(TypeUnitSound.SLIDER_EFFECT), 100);
+        }
+
+        [Test]
+        public void CheckChangeStateSoundOfType()
+        {
+            EngineSound sound = new EngineSound();
+
+            Assert.AreEqual(sound.GetStateUnitSound(TypeUnitSound.SLIDER_BACKGROUND), StateSlider.ON);
+            Assert.AreEqual(sound.GetStateUnitSound(TypeUnitSound.SLIDER_EFFECT), StateSlider.ON);
+
+            sound.SetStateUnitSound(TypeUnitSound.SLIDER_BACKGROUND, StateSlider.OFF);
+            sound.SetStateUnitSound(TypeUnitSound.SLIDER_EFFECT, StateSlider.OFF);
+
+            Assert.AreEqual(sound.GetStateUnitSound(TypeUnitSound.SLIDER_BACKGROUND), StateSlider.OFF);
+            Assert.AreEqual(sound.GetStateUnitSound(TypeUnitSound.SLIDER_EFFECT), StateSlider.OFF);
+        }
+
+    }
+}
